e17535c02ded9bc1ea8584645c94b37fe60b9d08
Alina Bimali <alinabimali36@gmail.com>
2026-06-25 16:01:07 +0545

    Added animation in the keys using js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML); 
+    buttonAnimation(buttonInnerHTML); 
   });
 }
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key); 
+  buttonAnimation(event.key);
 });
 
 
@@ -48,3 +50,15 @@ function makeSound(key) {
   }
 }
 
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    
+  
+    if (activeButton) {
+        activeButton.classList.add("pressed"); 
+
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
